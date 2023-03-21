6ce66c22b1fff7a04741fee44c8df91750a2b0be
Melvin <melvinmmelo@gmail.com>
2023-03-21 12:00:55 +0800

    access state in about page test

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import { useDispatch, useSelector } from 'react-redux';
+import { Route, Routes } from 'react-router';
 import { decrement, increment, setUser } from './actions';
 import './App.css';
+
+import Home from "./pages/Home"
+import About from "./pages/About";
+import { Link } from 'react-router-dom';
+
 
 function App() {
 
@@ -14,6 +20,21 @@ function App() {
 
   return (
     <div className="App">
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
       <h1>React Redux</h1>
 
       <h1>Counter {counter} </h1>
@@ -21,11 +42,12 @@ function App() {
       <button onClick={() => dispatch(increment())}> + </button>
       <button onClick={() => dispatch(decrement())}> - </button>
 
-      <button onClick={() => dispatch(setUser({ name: "Wow"}))}> SET USER</button>
+      <button onClick={() => dispatch(setUser({ name: "Wow" }))}>
+        {" "}
+        SET USER
+      </button>
 
-      { user && <p>{ user.name }</p>}
-
-
+      {user && <p>{user.name}</p>}
     </div>
   );
 }
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import { useSelector } from 'react-redux';
 
 function About() {
+
+  const user = useSelector((state) => state.user);
+
   return (
-    <div>About</div>
+    <div>About { user && user.name }</div>
   )
 }
 
